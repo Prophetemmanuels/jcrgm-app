1e4e1fb3b72244351cd8e446833c62ade0ac4c55
Prophetemmanuels <ghopbusinesscenter@gmail.com>
2026-01-17 07:27:51 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,24 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("jcrgm-cache").then(cache => {
-      return cache.addAll([
-        "index.html",
-        "manifest.json"
-      ]);
-    })
+const CACHE_NAME = "jcrgm-app-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./apple-touch-icon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener("fetch", event => {
+  if (event.request.url.startsWith("http")) {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+      })
+    );
+  }
 });
