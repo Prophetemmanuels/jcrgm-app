5bfff16417ceb20098064d608e12457cac134b06
Prophetemmanuels <ghopbusinesscenter@gmail.com>
2026-01-29 18:20:54 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,31 @@
-const CACHE_NAME = 'jcrgm-cache-v1';
+const CACHE_NAME = 'jcrgm-cache-v3';
+
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/apple-touch-icon.png',
-  '/icon-512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './apple-touch-icon.png',
+  './icon-512.png'
 ];
 
-// Install Service Worker
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
-});
-
-// Activate Service Worker
-self.addEventListener('activate', event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.keys().then(cacheNames => 
-      Promise.all(cacheNames.map(name => { if(name!==CACHE_NAME) return caches.delete(name); }))
-    )
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Fetch
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(names =>
+      Promise.all(names.map(n => n !== CACHE_NAME && caches.delete(n)))
+    )
+  );
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
 });
